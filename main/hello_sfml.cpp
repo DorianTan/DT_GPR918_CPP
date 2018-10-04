b0e58d2c611891ddea3254546dbf69c09f792381
Tan <doriantan7@gmail.com>
2018-10-04 13:39:39 +0200

    nouveaux ex

--- main/hello_sfml.cpp
+++ main/hello_sfml.cpp
@@ -6,9 +6,15 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "blabla");
+	sf::Sprite spriteTest;
+	sf::Texture textureTest;
 
-    // run the program as long as the window is open
+	if (!textureTest.loadFromFile("data/other_play.png")) {
+		return EXIT_FAILURE;
+	}
+	spriteTest.setTexture(textureTest);
+	// run the program as long as the window is open
     while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
@@ -21,6 +27,7 @@ int main()
         }
 
 		window.clear(sf::Color::Black);
+		window.draw(spriteTest);
 		window.display();
     }
     return EXIT_SUCCESS;
